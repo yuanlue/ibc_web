f94f9661b5e127f3b66caaba5e0308e00fb6161e
yuanlue <381043186@qq.com>
2018-08-10 22:43:32 +0800

    ibc

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,71 @@ router.get('/', async (ctx, next) => {
   })
 })
 
+router.get('/review', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!',
+    pagename:"review"
+  })
+})
+
 router.get('/about', async (ctx, next) => {
   await ctx.render('index', {
     title: 'Hello Koa 2!',
     pagename:"about"
+  })
+})
+router.get('/service/hotel', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!',
+    pagename:"service/hotel"
+  })
+})
+router.get('/about/modal', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!',
+    pagename:"about/modal"
+  })
+})
+router.get('/about/best', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!',
+    pagename:"about/best"
+  })
+})
+router.get('/service/cruise', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!',
+    pagename:"service/cruise"
+  })
+})
+router.get('/service/stays', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!',
+    pagename:"service/stay"
+  })
+})
+router.get('/service/group', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!',
+    pagename:"service/group"
+  })
+})
+router.get('/service/mice', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!',
+    pagename:"service/mice"
+  })
+})
+router.get('/review', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!',
+    pagename:"review"
+  })
+})
+router.get('/service', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!',
+    pagename:"service"
   })
 })
 
