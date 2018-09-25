49daa65592a68bb2d3e3d0f70401fd55119a33d3
yuanlue <381043186@qq.com>
2018-09-25 16:28:57 +0800

    route

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('koa-router')()
+const crypto = require('crypto')
 
 router.prefix('/users')
 
@@ -6,8 +7,23 @@ router.get('/', function (ctx, next) {
   ctx.body = 'this is a users response!'
 })
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
+router.get('/auth', function (ctx, next) {
+  console.log(ctx.query.id)
+  let token = '131420'
+  let signature = ctx.query.signature 
+  let timestamp = ctx.query.timestamp
+  let nonce = ctx.query.nonce
+  let arr = [token,timestamp,nonce].sort()
+  const sha1 = crypto.createHash('sha1')
+  sha1.update(arr.join(''))
+  const result = sha1.digest('hex')
+  if(result == signature){
+    ctx.body = ctx.query.echostr
+  }
+  else{
+    ctx.body = { code: -1, msg: "fail"}
+  }
+  //ctx.body = 'this is a users/bar response'
 })
 
 module.exports = router
