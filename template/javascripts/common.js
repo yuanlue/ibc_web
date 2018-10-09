b0a857d0b8e09b530c4f766921a61e077e831cac
Leo Wang <leo.wang@wendywuchina.com>
2018-10-09 12:46:01 +0800

    update form

--- template/javascripts/common.js
+++ template/javascripts/common.js
@@ -10,20 +10,6 @@ $(document).ready(function(){
     nextButton:'.swiper-button-next',
     
   })  
-    
-    switch(window.location.pathname)
-    {
-        case '/':
-            $('#homeli').addClass('active')
-            break;
-        case 'about.html':
-            $('#aboutli').addClass('active')
-            break;
-        case 'service.html':
-            $('#serviceli').addClass('active')
-            break;
-        case 'review.html':
-            $('#reviewli').addClass('active')
-            break;
-     }
+        
+     
 })
